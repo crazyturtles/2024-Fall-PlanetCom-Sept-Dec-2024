f577decb69734ed6dd788f200f5bb10ef9a73e52
Rohan Matharu <rohanmatharu@gmail.com>
2025-08-09 11:46:18 -0600

    Update page.tsx

--- PumpMaster-V1/pump_master/src/app/(pages)/reports/job-details/[jobId]/page.tsx
+++ PumpMaster-V1/pump_master/src/app/(pages)/reports/job-details/[jobId]/page.tsx
@@ -1,17 +1,17 @@
-// "use client";
+"use client";
 
-// import { ReportPage } from "@/app/Components/ReportsComponents/ReportPage";
-// import { jobDetailsConfig } from "@/app/Components/ReportsComponents/reports/jobDetailsConfig";
-// import { useParams } from "next/navigation";
+import { ReportPage } from "@/app/Components/ReportsComponents/ReportPage";
+import { jobDetailsConfig } from "@/app/Components/ReportsComponents/reports/jobDetailsConfig";
+import { useParams } from "next/navigation";
 
-// export default function JobDetailsPage() {
-// 	const params = useParams();
-// 	const jobId = params.jobId;
+export default function JobDetailsPage() {
+    const params = useParams();
+    const jobId = params.jobId;
 
-// 	const configWithJobId = {
-// 		...jobDetailsConfig,
-// 		baseEndpoint: `/api/reports/job-details/${jobId}`,
-// 	};
+    const configWithJobId = {
+        ...jobDetailsConfig,
+        baseEndpoint: `/api/reports/job-details/${jobId}`,
+    };
 
-// 	return <ReportPage config={configWithJobId} />;
-// }
+    return <ReportPage config={configWithJobId} />;
+}
